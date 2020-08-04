607ca52afae2bd6fea974ce169dcf1e6ce1ed2e1
Oryam <48219965+or-yam@users.noreply.github.com>
2020-08-04 09:14:39 +0300

    added walls and fixed move

--- dist/main.js
+++ dist/main.js
@@ -7,9 +7,9 @@ const board = new GoldRush(5, 5);
 board.loadGame();
 board.printMatrix();
 // console.log(board.matrix)
-// console.log('#########################')
-// board.move('up', board.player2);
-// board.move('left', board.player2);
-// board.move('down', board.player2);
+console.log('#########################')
+board.move('up', board.player2);
+board.move('left', board.player2);
+board.move('down', board.player2);
 
-// board.printMatrix();
+board.printMatrix();
--- dist/models/GoldRush.js
+++ dist/models/GoldRush.js
@@ -9,85 +9,69 @@ class GoldRush extends Matrix {
     this.player2 = { x: rows - 1, y: columns - 1, icon: 2, score: 0 };
     this.coinsRatio = 0.3;
     this.coins = Math.floor(rows * columns * this.coinsRatio);
+    this.wallsRatio = 0.2;
+    this.walls = Math.floor(rows * columns * this.wallsRatio);
   }
 
-  loadGame() {
-    this.matrix[0][0] = 1;
-    this.matrix[this.rowsNum - 1][this.columnsNum - 1] = 2;
+  loadPlayers() {
+    this.matrix[this.player1.x][this.player1.y] = this.player1.icon;
+    this.matrix[this.player2.x][this.player2.y] = this.player2.icon;
+  }
 
+  loadCoins() {
     for (let c = 0; c < this.coins; c++) {
       let x = Math.floor(Math.random() * Math.floor(this.rowsNum));
       let y = Math.floor(Math.random() * Math.floor(this.columnsNum));
-      if (
-        x !== 0 &&
-        y !== 0 &&
-        x !== this.rowsNum - 1 &&
-        y !== this.columnsNum
-      ) {
+      if (this.matrix[x][y] === '.') {
         this.matrix[x][y] = 'C';
       }
     }
   }
 
-  moveLeft(player) {
-    if (player.y === 0) {
-      console.log('canot move left');
-    } else {
-      this.matrix[player.x][player.y] = '.';
-      player.y--;
-      if (this.matrix[player.x][player.y] === 'C') {
-        player.score++;
+  loadWalls() {
+    for (let w = 0; w < this.walls; w++) {
+      let x = Math.floor(Math.random() * Math.floor(this.rowsNum));
+      let y = Math.floor(Math.random() * Math.floor(this.columnsNum));
+      if (this.matrix[x][y] === '.') {
+        this.matrix[x][y] = 'W';
       }
-      this.matrix[player.x][player.y] = player.icon;
-    }
-  }
-  moveRight(player) {
-    if (player.y === this.rowsNum - 1) {
-      console.log('canot move right');
-    } else {
-      this.matrix[player.x][player.y] = '.';
-      player.y++;
-      if (this.matrix[player.x][player.y] === 'C') {
-        player.score++;
-      }
-      this.matrix[player.x][player.y] = player.icon;
     }
   }
 
-  moveDown(player) {
-    if (player.x === this.columnsNum - 1) {
-      console.log('canot move down');
-    } else {
-      this.matrix[player.x][player.y] = '.';
-      player.x++;
-      if (this.matrix[player.x][player.y] === 'C') {
-        player.score++;
-      }
-      this.matrix[player.x][player.y] = player.icon;
-    }
-  }
-
-  moveUp(player) {
-    if (player.x === 0) {
-      console.log('canot move up');
-    } else {
-      this.matrix[player.x][player.y] = '.';
-      player.x--;
-      if (this.matrix[player.x][player.y] === 'C') {
-        player.score++;
-      }
-      this.matrix[player.x][player.y] = player.icon;
-    }
+  loadGame() {
+    this.loadPlayers();
+    this.loadWalls();
+    this.loadCoins();
   }
 
   move(direction, player) {
+    let newPos = { x: player.x, y: player.y };
     direction === 'left'
-      ? this.moveLeft(player)
+      ? newPos.y--
       : direction === 'right'
-      ? this.moveRight(player)
+      ? newPos.y++
       : direction === 'up'
-      ? this.moveUp(player)
-      : this.moveDown(player);
+      ? newPos.x--
+      : direction === 'down'
+      ? newPos.x++
+      : console.log('note valid direction');
+    if (this.matrix[newPos.x][newPos.y] === 'C') {
+      player.coins++;
+      this.matrix[player.x][player.y] = '.';
+      player.x = newPos.x;
+      player.y = newPos.y;
+      this.loadPlayers();
+    } else if (this.matrix[newPos.x][newPos.y] === '.') {
+      this.matrix[player.x][player.y] = '.';
+      player.x = newPos.x;
+      player.y = newPos.y;
+      this.loadPlayers();
+    } else if (
+      !this.matrix[newPos.x][newPos.y] ||
+      this.matrix[newPos.x][newPos.y] !== '.'
+    ) {
+      console.log(`im sorry im afraid i can't do that`);
+    }
   }
 }
 
