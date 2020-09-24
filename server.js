c585add1e7e7c22b1ecf6b5930a73daae45d2f4d
Oryam <48219965+or-yam@users.noreply.github.com>
2020-09-24 11:57:22 +0300

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-const PORT = 3300;
+const PORT = process.env.PORT || 3300;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
